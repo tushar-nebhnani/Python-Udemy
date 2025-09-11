c132e2529c320bd100423cea1f9d796a71692a6b
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2025-09-11 18:49:11 +0530

    Added source code. Error to debug.

--- Projects/04_url_shortner/main.py
+++ Projects/04_url_shortner/main.py
@@ -33,7 +33,18 @@ def index():
 def about():
     return f"This is me, Flask. Hello!"
 
+@app.route('/<short_code>')
+def redirect_url(short_code):
+    url_data = get_url(short_code)
+    if url_data:
+        increment_visit_count(short_code)
+        return redirect(url_data[1])
+    return render_template('404.html'), 404
 
+@app.route('/delete/<short_code>', methods=['POST'])
+def delete_url(short_code):
+    delete_url_by_code(short_code) 
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
