0fefc7449390e4e907aeb909b7e513cce0dcf9bc
Tushar Nebhnani <nebhnani.tushar8@gmail.com>
2025-09-14 12:07:00 +0530

    Start of PyDantic Journey

---
+++ Basics/11_pydantic/01_intro.py
@@ -0,0 +1,10 @@
+"""
+    Data Validation and Parsing. Setting and Configuration Management.
+    Data serilaization/deserilaization.
+
+    Stop the data interchangeablity of the data types.
+    name = "tushar"
+    name = 87 
+    ❌ This is not allowed in pydantic.
+"""
+print("Start of pydantic journey.")
